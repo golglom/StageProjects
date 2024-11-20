70759e86b6edb4621eea15cbf85fc5bff794ff21
georges-golden <156268532+golglom@users.noreply.github.com>
2024-11-20 00:20:19 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box")
+const listBox = document.getElementById("list-container")
+
+function addTask() {
+
+    if (inputBox.value == "") {
+        listBox.innerHTML = "";
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listBox.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+
+    inputBox.value = "";
+    sauvegarde();
+    
+}
+
+listBox.addEventListener('click', function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("accept")
+        sauvegarde();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        sauvegarde();
+    }
+},false)
+
+
+
+function sauvegarde() {
+    localStorage.setItem("data",listBox.innerHTML);
+}
+
+function récupération(){
+    listBox.innerHTML = localStorage.getItem("data");
+}
+récupération();
+
